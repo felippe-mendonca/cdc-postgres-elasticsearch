90a317ee08ca5413e1793cf425789059e64e6a8a
Felippe Mendonça <mendonca.felippe@gmail.com>
2021-08-01 19:47:59 -0300

    chore: adds create-flight sql script

---
+++ 2-create-flight.sql
@@ -0,0 +1,4 @@
+INSERT INTO flights(
+  flight_no, scheduled_departure, scheduled_arrival, departure_airport, arrival_airport, status, aircraft_code) 
+VALUES 
+  ('PG0461', '2017-09-05 09:35:00+00', '2017-09-05 10:20:00+00', 'SVO', 'ULV', 'Scheduled', 'SU9');
